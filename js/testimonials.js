0072acd2bf98e384dd2d0ff4c4b3ee8796328733
suryaa6666 <suryaelidanto@gmail.com>
2023-03-07 09:14:57 +0700

    fix(1.hof.js) : array and comment

--- js/testimonials.js
+++ js/testimonials.js
@@ -72,8 +72,7 @@
 
 // document.getElementById('testimonials').innerHTML = testimonialHTML;
 
-// 
-
+// array of objects
 const testimonialData = [
     {
         author: "Surya Elidanto",
@@ -98,7 +97,7 @@ const testimonialData = [
 function allTestimonials() {
     let testimonialHTML = '';
 
-    const testimonial = testimonialData.map(function (item) {
+    testimonialData.forEach(function (item) {
         testimonialHTML += `<div class="testimonial">
             <img src="${item.image}" class="profile-testimonial" />
             <p class="quote">"${item.quote}"</p>
@@ -126,7 +125,7 @@ function filterTestimonials(rating) {
     if (testimonialFiltered.length === 0) {
         testimonialHTML = `<h1> Data not found! </h1>`
     } else {
-        const testimonial = testimonialFiltered.map(function (item) {
+        testimonialFiltered.forEach(function (item) {
             testimonialHTML += `<div class="testimonial">
                 <img src="${item.image}" class="profile-testimonial" />
                 <p class="quote">"${item.quote}"</p>
