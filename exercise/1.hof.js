e9909da207b265269be33e6931fa9487157ec428
suryaa6666 <suryaelidanto@gmail.com>
2023-03-07 09:14:22 +0700

    result

--- exercise/1.hof.js
+++ exercise/1.hof.js
@@ -1,35 +1,29 @@
 // foreach
 const arr = [1, 2, 3, 4, 5];
-
 arr.forEach(function (item) {
     console.log(item);
 });
 
-//  map
-// const arr = [1, 2, 3, 4, 5];
+// map
+const arr2 = [1, 2, 3, 4, 5];
+const doubled = arr2.map((item) => {
+    return item * 2;
+});
 
-// const doubled = arr.map((item) => {
-//     return item * 2;
-// });
-
-// console.log(doubled);
+console.log(doubled);
 
 // filter
-// const arr = [1, 2, 3, 4, 5];
+const arr3 = [1, 2, 3, 4, 5];
+const evenNumbers = arr3.filter(function (item) {
+    return item % 2 === 0;
+});
 
-// const evenNumbers = arr.filter(function (item) {
-//     return item % 2 === 0;
-// });
-
-// console.log(evenNumbers);
-
-// Output: [2, 4]
+console.log(evenNumbers);
 
 // reduce
-// const arr = [1, 2, 3, 4, 5];
+const arr4 = [1, 2, 3, 4, 5];
+const sum = arr4.reduce(function (total, item) {
+    return total + item;
+}, 0);
 
-// const sum = arr.reduce(function (total, item) {
-//     return total + item;
-// }, 0);
-
-// console.log(sum);
+console.log(sum);
