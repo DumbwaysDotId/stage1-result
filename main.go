8b7c7e1c5b1115f07e2b72242d28c01e2d327170
suryaa6666 <suryaelidanto@gmail.com>
2023-03-14 08:50:41 +0700

    refactor(main.go) : simplify template usage

--- main.go
+++ main.go
@@ -2,20 +2,11 @@ package main
 
 import (
 	"html/template"
-	"io"
 	"net/http"
 	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
-
-type Template struct {
-	templates *template.Template
-}
-
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
 
 func main() {
 	// Create new Echo instance
@@ -27,12 +18,6 @@ func main() {
 
 	// Serve static files from "public" directory
 	e.Static("/public", "public")
-
-	t := &Template{
-		templates: template.Must(template.ParseGlob("views/*.html")),
-	}
-
-	e.Renderer = t
 
 	// Routing
 	e.GET("/hello", helloWorld)
@@ -49,19 +34,38 @@ func main() {
 }
 
 func helloWorld(c echo.Context) error {
+
 	return c.String(http.StatusOK, "Hello World")
 }
 
 func home(c echo.Context) error {
-	return c.Render(http.StatusOK, "index.html", nil)
+	var tmpl, err = template.ParseFiles("views/index.html")
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
+
+	return tmpl.Execute(c.Response(), nil)
 }
 
 func contact(c echo.Context) error {
-	return c.Render(http.StatusOK, "contact.html", nil)
+	var tmpl, err = template.ParseFiles("views/contact.html")
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
+
+	return tmpl.Execute(c.Response(), nil)
 }
 
 func blog(c echo.Context) error {
-	return c.Render(http.StatusOK, "blog.html", nil)
+	var tmpl, err = template.ParseFiles("views/blog.html")
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
+
+	return tmpl.Execute(c.Response(), nil)
 }
 
 func blogDetail(c echo.Context) error {
@@ -73,11 +77,23 @@ func blogDetail(c echo.Context) error {
 		"Content": "REPUBLIKA.CO.ID, JAKARTA -- Ketimpangan sumber daya manusia (SDM) disektor digital masih menjadi isu yang belum terpecahkan. Berdasarkan penelitian ManpowerGroup.REPUBLIKA.CO.ID, JAKARTA -- Ketimpangan sumber daya manusia (SDM) disektor digital masih menjadi isu yang belum terpecahkan. Berdasarkan pen...",
 	}
 
-	return c.Render(http.StatusOK, "blog-detail.html", data)
+	var tmpl, err = template.ParseFiles("views/blog-detail.html")
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
+
+	return tmpl.Execute(c.Response(), data)
 }
 
 func formAddBlog(c echo.Context) error {
-	return c.Render(http.StatusOK, "add-blog.html", nil)
+	var tmpl, err = template.ParseFiles("views/add-blog.html")
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
+
+	return tmpl.Execute(c.Response(), nil)
 }
 
 func addBlog(c echo.Context) error {
